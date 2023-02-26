fec57db2162e3c120c228d757a2f086435e99b58
jimender2 <35303639+jimender2@users.noreply.github.com>
2023-02-25 23:42:04 -0500

    Add git module.

--- git_of_theseus.go
+++ git_of_theseus.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+)
+
+var ignoreFileTypes [14]string = [14]string{
+	"*.json",
+	"*.md",
+	"*.ps",
+	"*.eps",
+	"*.txt",
+	"*.xml",
+	"*.xsl",
+	"*.rss",
+	"*.xslt",
+	"*.xsd",
+	"*.wsdl",
+	"*.wsf",
+	"*.yaml",
+	"*.yml",
+}
 
 func main() {
 	fmt.Println("Hello, world.")
+
+	// loop through everything in ignoreFileTypes
+	for _, ignoreFileType := range ignoreFileTypes {
+		fmt.Println(ignoreFileType)
+	}
+
+	_, err := git.PlainOpen("")
+
+	if err != nil {
+		fmt.Println("test")
+	} else {
+		fmt.Println("2")
+	}
+
 }
