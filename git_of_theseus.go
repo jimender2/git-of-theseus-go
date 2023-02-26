f3f98dd19eefa54c9eeb7711617e638575b4a6ec
jimender2 <35303639+jimender2@users.noreply.github.com>
2023-02-26 00:33:34 -0500

    Loop through all commits in the repo

--- git_of_theseus.go
+++ git_of_theseus.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 var ignoreFileTypes [14]string = [14]string{
@@ -27,7 +28,9 @@ var ignoreFileTypes [14]string = [14]string{
 
 func main() {
 	var gitRepoPath string
+	var gitRepoBranch string
 	flag.StringVar(&gitRepoPath, "repo", ".", "Git repository path")
+	flag.StringVar(&gitRepoBranch, "branch", "master", "Git branch name")
 	flag.Parse()
 
 	fmt.Println(gitRepoPath)
@@ -41,5 +44,31 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Println("testing git repository")
+	fmt.Println("Opening git repository branch")
+	fmt.Println("Implementing later")
+	// fmt.Println(gitRepoBranch)
+	// gitRepo.Branch(gitRepoBranch)
+	// if err != nil {
+	// 	fmt.Println("Unable to find the branch specified. Falling back to the default branch")
+	// }
+	// vh, err := gitRepo.Head()
+	// fmt.Println(vh)
+	// fmt.Println(err)
+
+	fmt.Println("Looping through all commits.")
+	ref, err := gitRepo.Head()
+
+	cIter, err := gitRepo.Log(&git.LogOptions{From: ref.Hash()})
+
+	var count = 0
+	// ... just iterates over the commits, printing it
+	err = cIter.ForEach(func(c *object.Commit) error {
+		// fmt.Println(c)
+		count = count + 1
+		return nil
+	})
+
+	fmt.Println(count)
+
+	fmt.Println("end")
 }
